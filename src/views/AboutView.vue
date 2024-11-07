17ca6c296202fcaa6cd100a858840bd7d085edd3
周小彪 <1360600396@qq.com>
2024-11-07 22:04:34 +0800

    zxb

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,83 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <a-dropdown>
+    <a-menu slot="overlay" @click="handleMenuClick">
+      <a-menu-item key="1">
+        <a-icon type="user" />1st menu item
+      </a-menu-item>
+      <a-menu-item key="2">
+        <a-icon type="user" />2nd menu item
+      </a-menu-item>
+      <a-menu-item key="3">
+        <a-icon type="user" />3rd item
+      </a-menu-item>
+    </a-menu>
+    <a-button style="margin-left: 8px">
+      {{ selectedMenuText }} <a-icon type="down" />
+    </a-button>
+  </a-dropdown>
 </template>
+
+<script>
+import { Dropdown, Menu, MenuItem, Icon, Button } from 'ant-design-vue';
+
+export default {
+  components: {
+    Dropdown,
+    Menu,
+    MenuItem,
+    Icon,
+    Button,
+  },
+  data() {
+    return {
+      selectedMenuText: 'Select an option', // 默认文本
+    };
+  },
+  methods: {
+    handleMenuClick(e) {
+      const { key } = e; // 获取点击的菜单项的 key
+
+      // 更新按钮文本
+      const menuItems = {
+        1: '1st menu item',
+        2: '2nd menu item',
+        3: '3rd item',
+      };
+      this.selectedMenuText = menuItems[key];
+
+      // 根据点击的菜单项执行不同的操作
+      switch (key) {
+        case '1':
+          this.handleFirstOption();
+          break;
+        case '2':
+          this.handleSecondOption();
+          break;
+        case '3':
+          this.handleThirdOption();
+          break;
+        default:
+          break;
+      }
+    },
+
+    // 定义每个菜单项对应的方法
+    handleFirstOption() {
+      console.log('1');
+      // 在这里写你处理第一个选项的逻辑
+    },
+    handleSecondOption() {
+      console.log('2');
+      // 在这里写你处理第二个选项的逻辑
+    },
+    handleThirdOption() {
+      console.log('3');
+      // 在这里写你处理第三个选项的逻辑
+    },
+  },
+};
+</script>
+
+<style>
+@import 'ant-design-vue/dist/antd.css';
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,346 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    
+		<div class="condition-box">
+			<div class="title-box">
+				<div 
+					v-for="(item, index) in titles" 
+					:key="index"	
+					:class="current == index ? 'active' : ''"
+					@click="changeTitle(item, index)"
+				>
+					{{ item.title }}
+					<span> {{ item.subTitle }} </span>
+				</div>
+			</div>
+			<div class="search-box">
+				<a-input-search
+				  v-model="value"
+				  placeholder="input search text"
+				  style="width: 200px"
+				  @search="onSearch"
+				/>
+			</div>
+		</div>
+		<div class="list-box">
+			<div class="list-item" v-for="(item, index) in pics" :key="index">
+				<div class="list-item-box">
+					<div class="tips">{{ item.tips }}</div>
+					<img v-lazy='imgUrl' alt="">
+					<div class="list-desc">
+						<div class="title">{{ item.title }}</div>
+						<div class="time">{{ item.time }}</div>
+					</div>
+				</div>
+				<a-dropdown-button @click="handleButtonClick">
+      Dropdown
+      <a-menu slot="overlay" @click="handleMenuClick">
+        <a-menu-item key="1"> <a-icon type="user" />1st menu item </a-menu-item>
+        <a-menu-item key="2"> <a-icon type="user" />2nd menu item </a-menu-item>
+        <a-menu-item key="3"> <a-icon type="user" />3rd item </a-menu-item>
+      </a-menu>
+    </a-dropdown-button>
+			</div>
+		</div>
+	
+		
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { Input } from 'ant-design-vue';
+import {
+  Button,
+  Icon,
+  Menu,
+  Modal,
+  Badge,
+  message,
+  Tooltip,
+} from "ant-design-vue";
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    'a-input-search': Input.Search,
+		AMenu: Menu,
+    AMenuItemGroup: Menu.ItemGroup,
+    AMenuItem: Menu.Item,
+    ASubMenu: Menu.SubMenu,
+    AMenuDivider: Menu.Divider,
+    AButton: Button,
+    ATooltip: Tooltip,
+    AModal: Modal,
+  
+  },
+    data() {
+			return {
+				imgUrl: "https://build.glodonedu.com/oss/image/byid/3574132519423513066",
+				titles: [
+					{id: 1, title: "软件学习", subTitle: "123"},
+					{id: 2, title: "专家课堂", subTitle: "123"},
+					{id: 3, title: "直播回顾", subTitle: "123"},
+				],
+				current: 0,
+				value: "", // 搜索框的值
+				pics: [
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+					{ id: 1, title: "9期-方圆-群体工程穿插施工分析及进度管理", tips: "小标签", time: "时长：30分钟"},
+				]
+			}
+		},
+		methods: {
+			onSearch: function(searchValue){
+			  console.log('use value', searchValue);
+			  console.log('or use this.value', this.value);
+			},
+			changeTitle(item, index) {
+				this.current = index;
+			}
+		}
+	}
 </script>
+
+<style scoped>
+	.active {
+		border: 3px solid blue;
+	}
+	
+	
+	.condition-box {
+		width: 100%;
+		height: 80px;
+		padding: 0 10px;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+	
+	.title-box {
+		display: flex;
+		border-radius: 10px;
+		
+	}
+	
+	.title-box div {
+		width: 160px;
+		height: 60px;
+		font-size: 20px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		color: white;
+	}
+	
+	.title-box div span {
+		font-size: 12px;
+	}
+	
+	.title-box div:nth-child(1) {
+		background-color: #fc6a6b;
+	}
+	
+	
+	.title-box div:nth-child(2) {
+		background-color: #45cad9;
+	}
+	.title-box div:nth-child(3) {
+		background-color: #f5c45e;
+	}
+	
+	
+	.list-box {
+		width: 100%;
+		display: flex;
+		flex-wrap: wrap;
+	}
+	.list-item {
+		width: 20%;
+		padding: 10px;
+		box-sizing: border-box;
+	}
+	
+	.list-item-box {
+		width: 100%;
+		border: 1px solid #ccc; 
+		border-radius: 10px;
+		position: relative;
+	}
+	
+	.tips {
+		padding: 4px 12px;
+		color: white;
+		position: absolute;
+		top: 0px;
+		right: 0px;
+		background-color: deepskyblue;
+		border-bottom-left-radius: 10px;
+		border-top-right-radius: 10px;
+	}
+	
+	img {
+		width: 100%;
+		border-top-left-radius: 10px;
+		border-top-right-radius: 10px;
+		transition: .2s;
+	}
+	img:hover {
+		transform: scale(1.1);
+	}
+	
+	.list-desc {
+		display: flex;
+		width: 100%;
+		height: 40px;
+		padding: 0 10px;
+		align-items: center;
+		justify-content: space-between;
+		
+		
+	}
+	
+	.title {
+		font-size: 14px;
+		font-weight: 600;
+		width: 70%;
+		white-space: nowrap;  /* 强制不换行 */
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+	
+	.time {
+		width: 30%;
+		font-size: 12px;
+	}
+	
+	/* 当屏幕小于1400px */
+	@media screen and (max-width: 1400px){
+		.list-box {
+		}
+		.list-desc {
+			
+			
+		}
+		
+		.tips {
+			padding: 4px 8px;
+			color: white;
+			position: absolute;
+			top: 0px;
+			right: 0px;
+			background-color: deepskyblue;
+			border-bottom-left-radius: 10px;
+			border-top-right-radius: 10px;
+		}
+		
+		.title {
+			font-size: 14px !important;
+			width: 60% !important;
+		}
+		.time {
+			font-size: 12px;
+			width: 40% !important;
+		}
+	}
+	/* 当屏幕小于1200px */
+	@media screen and (max-width: 1200px){
+		.list-box {
+		}
+		.list-item {
+			width: 25%;
+		}
+	}
+	
+	/* 当屏幕小于992px */
+	@media screen and (max-width: 992px){
+		.list-box {
+		}
+		
+		.list-desc {
+			display: flex;
+			flex-wrap: wrap;
+			
+		}
+		.tips {
+			padding: 4px 8px;
+			font-size: 12px;
+			color: white;
+			position: absolute;
+			top: 0px;
+			right: 0px;
+			background-color: deepskyblue;
+			border-bottom-left-radius: 10px;
+			border-top-right-radius: 10px;
+		}
+		.title {
+			font-size: 14px !important;
+			width: 100% !important;
+		}
+		.time {
+			font-size: 12px;
+			width: 100% !important;
+		}
+		
+		
+	}
+	/* 当屏幕小于768 */
+	@media screen and (max-width: 768px){
+		.list-box {
+		}
+		.list-item {
+			width: 33.3%;
+		}
+	}
+	
+	
+	/* 当屏幕小于700 */
+	@media screen and (max-width: 700px){
+		.list-box {
+			}
+		.list-item {
+			width: 50%;
+		}
+		.condition-box {
+			display: flex;
+			flex-wrap: wrap;
+			margin: 40px 0;
+		}
+		.title-box { 
+			width: 100%;
+		}
+		.search-box {
+			width: 100%;
+			margin-top: 20px;
+			
+		}
+	}
+	
+	
+</style>
